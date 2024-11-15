821d69dca079bdd8b387b8d2a094aba2586e840b
Lucas Goebel <pro.lcsgoebel@gmail.com>
2024-11-15 20:31:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# estudo-PHP
+ Repositorio criado com foco em entender o PHP para que seja utilizado em um CRUD
---
+++ guanabara/helloWorld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+    $nome="Lucas";
+    echo "<p>Eu me chamo $nome!</p>";
+?> 
+
+</body>
+</html> 
